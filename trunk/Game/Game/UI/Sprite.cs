468b6c1cfab37b8d9faf2183675d0e0f9cb672fc
asamgdla@gmail.com <asamgdla@gmail.com@13dcf251-5519-c905-372b-15839a5b83af>
2012-07-31 21:15:26 +0000

    update issue 23 Status: New Added a Sprite class to use it for drawing different textures.

---
+++ trunk/Game/Game/UI/Sprite.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Game.UI
+{
+    class Sprite
+    {
+        Texture2D texture;
+        Rectangle area;
+
+        public Sprite(Texture2D tex, Rectangle area)
+        {
+            this.texture = tex;
+            this.area = area;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            spriteBatch.Draw(texture, area, Color.White);
+            spriteBatch.End();
+        }
+    }
+}

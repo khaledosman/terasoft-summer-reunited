6000a9a91c39b4f9474c988c32892ec2ab9ad936
vendettax6@gmail.com <vendettax6@gmail.com@13dcf251-5519-c905-372b-15839a5b83af>
2012-08-01 19:45:15 +0000

    update issue 26 Status: Started uploaded class Player

---
+++ trunk/Game/Game/Engine/Player.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Game.UI;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Game.Engine
+{
+    
+
+    class Player
+    {
+        //Enumerator on player state
+        enum PlayerState
+        {
+            Running,
+            Jumping, 
+            Sliding, 
+            Dying
+        }
+
+        //
+        PlayerState State;
+
+        //Animation of the avatar.
+        SpriteAnimation playerAnimation;
+        SpriteAnimation runAnimation;
+        SpriteAnimation jumpAnimation;
+
+        //Sprite Textures
+        Texture2D runTexture, jumpTexture;
+
+        //Avatar's Position on screen.
+        Vector2 Position;
+
+        //Player health.
+        private int immunity;
+
+        //Player Score
+        private int score;
+
+        //Player items status
+        bool hasShield, hasSword;
+
+        public Player()
+        {
+            Position = new Vector2();
+            State = PlayerState.Running;
+            runAnimation = new SpriteAnimation();
+            jumpAnimation = new SpriteAnimation();
+        }
+
+        public void LoadContent(ContentManager Content)
+        {
+            runTexture = Content.Load<Texture2D>("sprites/run");
+            jumpTexture = Content.Load<Texture2D>("sprites/jump");
+        }
+
+        public void Initialize()
+        {
+            runAnimation.Initialize(runTexture, Position, 0.1f, Color.White, 1f, true);
+            jumpAnimation.Initialize(jumpTexture, Position, 0.1f, Color.White, 1f, false);
+            playerAnimation = runAnimation; 
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            switch (State)
+            {
+                case PlayerState.Running: playerAnimation = runAnimation; break;
+                case PlayerState.Jumping: playerAnimation = jumpAnimation; break;
+                default: playerAnimation = runAnimation; break;
+            }
+            playerAnimation.Update(gameTime);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            playerAnimation.Draw(spriteBatch);
+        }
+
+        public void Collided(int value)
+        {
+            immunity += value;
+        }
+    }
+}

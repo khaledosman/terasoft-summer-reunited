5296332b7d354c28c52583061b42f5a975c8745d
asamgdla@gmail.com <asamgdla@gmail.com@13dcf251-5519-c905-372b-15839a5b83af>
2012-08-01 15:42:37 +0000

    update issue 9 Status: New Added the Sword class.

---
+++ Game/Game/Engine/Sword.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game.Engine
+{
+    class Sword : Collectible
+    {
+        /// <summary>
+        /// Author: Ahmed Shirin.
+        /// </summary>
+        public Sword(string name, bool acquired) : base(acquired, name)
+        {
+
+        }
+
+        public void AcquireSword()
+        {
+            base.AcquireObject();
+        }
+    }
+}

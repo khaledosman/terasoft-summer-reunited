66e9df4dd98d73547e55065570a4b7e61325174b
vendettax6@gmail.com <vendettax6@gmail.com@13dcf251-5519-c905-372b-15839a5b83af>
2012-08-01 19:43:29 +0000

    update issue 29 uploaded class SpriteAnimation

--- trunk/Game/Game/UI/SpriteAnimation.cs
+++ trunk/Game/Game/UI/SpriteAnimation.cs
@@ -8,36 +8,142 @@ namespace Game.UI
     public class SpriteAnimation
     {
 
-        public int FramesPerSecond { get; private set; }
-
-        public Texture2D Texture { get; set; }
-
-        public Rectangle SourceRectangle { get; set; }
+        // The image representing the collection of images used for animation
+        Texture2D spriteStrip;
 
 
-        public void Initialize(int framesPerSecond)
+        // The scale used to display the sprite strip
+        float scale;
+
+
+        // The time since we last updated the frame
+        int elapsedTime;
+
+
+        // The time we display a frame until the next one
+        float frameTime;
+
+
+        // The number of frames that the animation contains
+        int frameCount;
+
+
+        // The index of the current frame we are displaying
+        int currentFrame;
+
+
+        // The color of the frame we will be displaying
+        Color color;
+
+
+        // The area of the image strip we want to display
+        Rectangle sourceRect = new Rectangle();
+
+
+        // The area where we want to display the image strip in the game
+        Rectangle destinationRect = new Rectangle();
+
+
+        // Width of a given frame
+        public int FrameWidth;
+
+
+        // Height of a given frame
+        public int FrameHeight;
+
+
+        // The state of the Animation
+        public bool Active;
+
+
+        // Determines if the animation will keep playing or deactivate after one run
+        public bool Looping;
+
+
+        // Width of a given frame
+        public Vector2 Position;
+
+
+        public void Initialize(Texture2D texture, Vector2 position,
+float frametime, Color color, float scale, bool looping)
         {
+            // Keep a local copy of the values passed in
+            this.color = color;
+            this.FrameWidth = texture.Height;
+            this.FrameHeight = texture.Height;
+            this.frameCount = texture.Width/FrameWidth;
+            this.frameTime = frametime;
+            this.scale = scale;
 
-        }
 
-        public void LoadContent(TextureList textureList)
-        {
+            Looping = looping;
+            Position = position;
+            spriteStrip = texture;
 
+
+            // Set the time to zero
+            elapsedTime = 0;
+            currentFrame = 0;
+
+
+            // Set the Animation to active by default
+            Active = true;
         }
 
         public void Update(GameTime gameTime)
         {
+            // Do not update the game if we are not active
+            if (Active == false)
+                return;
 
+
+            // Update the elapsed time
+            elapsedTime += (int)gameTime.ElapsedGameTime.TotalMilliseconds;
+
+
+            // If the elapsed time is larger than the frame time
+            // we need to switch frames
+            if (elapsedTime > frameTime)
+            {
+                // Move to the next frame
+                currentFrame++;
+
+
+                // If the currentFrame is equal to frameCount reset currentFrame to zero
+                if (currentFrame == frameCount)
+                {
+                    currentFrame = 0;
+                    // If we are not looping deactivate the animation
+                    if (Looping == false)
+                        Active = false;
+                }
+
+
+                // Reset the elapsed time to zero
+                elapsedTime = 0;
+            }
+
+
+            // Grab the correct frame in the image strip by multiplying the currentFrame index by the frame width
+            sourceRect = new Rectangle(currentFrame * FrameWidth, 0, FrameWidth, FrameHeight);
+
+
+            // Grab the correct frame in the image strip by multiplying the currentFrame index by the frame width
+            destinationRect = new Rectangle((int)Position.X - (int)(FrameWidth * scale) / 2,
+            (int)Position.Y - (int)(FrameHeight * scale) / 2,
+            (int)(FrameWidth * scale),
+            (int)(FrameHeight * scale));
         }
 
-        public void Start()
+
+        // Draw the Animation Strip
+        public void Draw(SpriteBatch spriteBatch)
         {
-
-        }
-
-        public void Stop()
-        {
-
+            // Only draw the animation when we are active
+            if (Active)
+            {
+                spriteBatch.Draw(spriteStrip, destinationRect, sourceRect, color);
+            }
         }
 
     }

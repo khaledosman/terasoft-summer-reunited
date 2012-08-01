bc88eea7a58ef114124f6f799c99cc5dc216334a
asamgdla@gmail.com <asamgdla@gmail.com@13dcf251-5519-c905-372b-15839a5b83af>
2012-08-01 21:20:13 +0000

    update issue 23 Status: New Updated the MainScreen class to load and draw the main menu.

--- Game/Game/Screens/MainScreen.cs
+++ Game/Game/Screens/MainScreen.cs
@@ -1,10 +1,56 @@
 ﻿using Game.UI;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Game.Screens
 {
     public class MainScreen : GameScreen
     {
+        Sprite menu;
+        Sprite newGame;
+        Sprite instructions;
+        Sprite exit;
+        Sprite newGameLabel;
+        Sprite instructionsLabel;
+        Sprite exitLabel;
+        GraphicsDeviceManager graphics;
 
+        public MainScreen(GraphicsDeviceManager graphics)
+        {
+            this.graphics = graphics;
+            this.graphics.PreferredBackBufferWidth = 1280;
+            this.graphics.PreferredBackBufferHeight = 720;
+            this.graphics.IsFullScreen = true;
+        }
+
+        public void Initialize()
+        {
+
+        }
+
+        public void LoadContent(ContentManager Content)
+        {
+            menu = new Sprite(Content.Load<Texture2D>("Textures//menu"), new Rectangle(0, 0, 1280, 720));
+            newGame = new Sprite(Content.Load<Texture2D>("Textures//new"), new Rectangle(140, 230, 200, 200));
+            instructions = new Sprite(Content.Load<Texture2D>("Textures//instructions"), new Rectangle(540, 230, 200, 200));
+            exit = new Sprite(Content.Load<Texture2D>("Textures//exit"), new Rectangle(940, 230, 200, 200));
+            newGameLabel = new Sprite(Content.Load<Texture2D>("Textures//new_label"), new Rectangle(140, 450, 200, 50));
+            instructionsLabel = new Sprite(Content.Load<Texture2D>("Textures//instructions_label"), new Rectangle(545, 450, 200, 50));
+            exitLabel = new Sprite(Content.Load<Texture2D>("Textures//exit_label"), new Rectangle(950, 450, 200, 50));
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            menu.Draw(spriteBatch);
+            newGame.Draw(spriteBatch);
+            instructions.Draw(spriteBatch);
+            exit.Draw(spriteBatch);
+            newGameLabel.Draw(spriteBatch);
+            instructionsLabel.Draw(spriteBatch);
+            exitLabel.Draw(spriteBatch);
+        }
 
     }
 }

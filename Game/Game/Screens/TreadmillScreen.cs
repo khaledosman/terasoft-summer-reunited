f9adfd4a4f07318ca3ffb6eac08219047a7e969a
asamgdla@gmail.com <asamgdla@gmail.com@13dcf251-5519-c905-372b-15839a5b83af>
2012-08-02 23:21:53 +0000

    update issue 22 Status: Started Added the TreamdmillScreen class.

---
+++ Game/Game/Screens/TreadmillScreen.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Game.UI;
+
+namespace Game.Screens
+{
+    public class TreadmillScreen : GameScreen
+    {
+
+
+    }
+}

b5550e942025ae9b2f5d81bb78ea21e69b58c648
khaled.osmaan@gmail.com <khaled.osmaan@gmail.com@13dcf251-5519-c905-372b-15839a5b83af>
2012-08-02 02:27:44 +0000

    Update issue 8 Added class ScreenManager taken from the other summer project temporarily, it needs some improvements

--- trunk/Game/Game/UI/ScreeenManager.cs
+++ trunk/Game/Game/UI/ScreeenManager.cs
@@ -1,21 +1,119 @@
 ﻿using System.Collections.Generic;
-
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Game.UI
 {
-    public class ScreanManager
+    public class ScreenManager : DrawableGameComponent
     {
+        private List<GameScreen> screens = new List<GameScreen>();
+        private List<GameScreen> screensToUpdate = new List<GameScreen>();
+        private SpriteBatch spriteBatch;
 
-        Stack<GameScreen> screens;
-
-        public void AddScreen(GameScreen screen)
+        /// <summary>
+        /// Returns the sprite batch object.
+        /// </summary>
+        public SpriteBatch SpriteBatch
         {
+            get { return spriteBatch; }
+        }
+
+        /// <summary>
+        /// Creates a new instance of ScreenManager.
+        /// </summary>
+        public ScreenManager(Microsoft.Xna.Framework.Game game)
+            : base(game)
+        {
+            base.Initialize();
 
         }
 
-        public void RemoveScreen()
+        /// <summary>
+        /// Loads the content of the screens managed by the screenManager.
+        /// </summary>
+        protected override void LoadContent()
         {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            foreach (GameScreen screen in screens)
+                screen.LoadContent();
+        }
 
+        /// <summary>
+        /// Unloads the content of the screens managed by the screenManager.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            foreach (GameScreen screen in screens)
+                screen.UnloadContent();
+        }
+        /// <summary>
+        /// Updates the screens managed by the screenManager.
+        /// </summary>
+        /// <param name="gameTime">Represents the time of the game.</param>
+        public override void Update(GameTime gameTime)
+        {
+            screensToUpdate.Clear();
+
+
+            foreach (GameScreen screen in screens)
+            {
+                if (!screen.IsFrozen)
+                    screensToUpdate.Add(screen);
+            }
+
+            if (screensToUpdate.Count == 0)
+                foreach (GameScreen screen in screens)
+                {
+                    screen.UnfreezeScreen();
+                    screensToUpdate.Add(screen);
+                }
+            else
+            {
+                while (screensToUpdate.Count > 0)
+                {
+                    GameScreen screen = screensToUpdate[screensToUpdate.Count - 1];
+
+                    screensToUpdate.RemoveAt(screensToUpdate.Count - 1);
+                    screen.Update(gameTime);
+                }
+            }
+        }
+        /// <summary>
+        /// Updates the screens managed by the screenManager.
+        /// </summary>
+        /// <param name="gameTime">Represents the time of the game.</param>
+        public override void Draw(GameTime gameTime)
+        {
+            foreach (GameScreen screen in screens)
+            {
+                if (screen.ScreenState == ScreenState.Hidden)
+                    continue;
+
+                screen.Draw(gameTime);
+            }
+        }
+        /// <summary>
+        /// Adds a screen to the list of screens managed by the screenManager.
+        /// </summary>
+        /// <param name="screen">Represents the screen that should be managed by the screenManager.</param>
+        public void AddScreen(GameScreen screen)
+        {
+            screen.ScreenManager = this;
+            screen.Initialize();
+            screen.LoadContent();
+            screens.Add(screen);
+        }
+
+        /// <summary>
+        /// Removes a screen from the list of screens managed by the screenManager.
+        /// </summary>
+        /// <param name="screen">Represents the screen that should be removed from the list
+        /// of screens managed by the screenManager.</param>
+        public void RemoveScreen(GameScreen screen)
+        {
+            screen.UnloadContent();
+            screens.Remove(screen);
+            screensToUpdate.Remove(screen);
         }
 
     }

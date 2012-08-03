fa86666da721d6a0935efa896ec390ce3536116c
khaled.osmaan@gmail.com <khaled.osmaan@gmail.com@13dcf251-5519-c905-372b-15839a5b83af>
2012-08-03 09:45:17 +0000

    Update issue 2 adding class PunchGesture which detects the punch gesture

---
+++ trunk/Game/Game/Kinect/PunchGesture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Kinect;
+
+namespace Game.Kinect
+{
+    class PunchGesture : IRelativeGestureSegment
+    {
+        public GesturePartResult CheckGesture(Skeleton skeleton)
+        {
+            if (skeleton.Joints[JointType.HandRight].Position.Y >= skeleton.Joints[JointType.ElbowRight].Position.Y)
+            {
+                if (skeleton.Joints[JointType.HandRight].Position.Z > (skeleton.Joints[JointType.ElbowRight].Position.Z))
+                {
+                    return GesturePartResult.Suceed;
+                }
+                return GesturePartResult.Pausing;
+            }
+                return GesturePartResult.Fail;
+            }
+        }
+
+    }
+

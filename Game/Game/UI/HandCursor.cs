ef631deda32d73475b3258037c2daa93e29eaf74
vendettax6@gmail.com <vendettax6@gmail.com@13dcf251-5519-c905-372b-15839a5b83af>
2012-08-03 00:28:30 +0000

    update issue 25 Added HandCursor

---
+++ Game/Game/UI/HandCursor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
+using Microsoft.Kinect;
+using Game.Kinect;
+
+namespace Game.UI
+{
+    class HandCursor
+    {
+        private Texture2D texture;
+        private Kinect.Kinect kinect;
+        private Vector2 position;
+
+        public HandCursor()
+        {
+
+        }
+
+        public void Initialize(Kinect.Kinect kinect)
+        {
+            this.kinect = kinect;
+        }
+
+        public void LoadContent(ContentManager Content)
+        {
+            texture = Content.Load<Texture2D>("Textures/HandImage");
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            Joint RightHand = kinect.GetCursorPosition();
+            position = new Vector2(RightHand.Position.X, RightHand.Position.Y);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, Color.White);
+        }
+    }
+}

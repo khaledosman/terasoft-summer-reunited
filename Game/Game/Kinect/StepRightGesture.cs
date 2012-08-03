1d88019b810e7e8ab7731eefae992dd427794484
khaled.osmaan@gmail.com <khaled.osmaan@gmail.com@13dcf251-5519-c905-372b-15839a5b83af>
2012-08-03 09:56:01 +0000

    Update issue 3 Added implementation of the step right gesture

---
+++ Game/Game/Kinect/StepRightGesture.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Kinect;
+
+namespace Game.Kinect
+{
+    public class StepRightGesture
+    {
+        static double posX;
+        class StepRightGesture1 : IRelativeGestureSegment
+        {
+            public GesturePartResult CheckGesture(Skeleton skeleton)
+            {
+                posX = skeleton.Joints[JointType.HipCenter].Position.Z;
+                if (skeleton.Joints[JointType.Head].Position.X == skeleton.Joints[JointType.HipCenter].Position.X)
+                {
+                    if (skeleton.Joints[JointType.Head].Position.Z == (skeleton.Joints[JointType.HipCenter].Position.Z))
+                    {
+                        return GesturePartResult.Suceed;
+                    }
+                    return GesturePartResult.Pausing;
+                }
+                return GesturePartResult.Fail;
+            }
+            class StepRightGesture2 : IRelativeGestureSegment
+            {
+                public GesturePartResult CheckGesture(Skeleton skeleton)
+                {
+                    if (skeleton.Joints[JointType.Head].Position.X >posX)
+                    {
+                        if (skeleton.Joints[JointType.HipCenter].Position.X >posX)
+                        {
+                            return GesturePartResult.Suceed;
+                        }
+                        return GesturePartResult.Pausing;
+                    }
+                    return GesturePartResult.Fail;
+                }
+
+            }
+        }
+    }
+}

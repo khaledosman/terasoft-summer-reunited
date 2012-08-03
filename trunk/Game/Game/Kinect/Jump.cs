14419cd34b903043e4ccdb065c19721fa4dc67eb
hollaxyman@gmail.com <hollaxyman@gmail.com@13dcf251-5519-c905-372b-15839a5b83af>
2012-08-03 22:52:42 +0000

    update issue 11 Status: Started
    Added the implementation of jump.However eqn for elevation angle still needed to be added

---
+++ trunk/Game/Game/Kinect/Jump.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Kinect;
+namespace Game.Kinect
+{
+    class Jump
+    {
+        public bool flag = false;
+        public void skelation(Skeleton s)
+        {
+            //Angle used 8 ,eqn needed for tilt angle 
+            //3ady head position : 0.4 ,if i jumped got 0.6 to 0.7,if(n>0.5) i jump,diff between head to hip is 0.6
+            double diff = s.Joints[JointType.Head].Position.Y - s.Joints[JointType.HipCenter].Position.Y;
+            double n = (diff * 0.88);
+            if (s.Joints[JointType.Head].Position.Y > n)
+                flag = true;
+        }
+    }
+}

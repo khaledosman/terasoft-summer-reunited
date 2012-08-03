a4e91e998fc3ad80cb2a54fc3c8e5cd4c3e448ef
asamgdla@gmail.com <asamgdla@gmail.com@13dcf251-5519-c905-372b-15839a5b83af>
2012-08-03 02:29:48 +0000

    update issue 9 Status: Started Modified all the classes representing items in the game.

--- trunk/Game/Game/Engine/Gym.cs
+++ trunk/Game/Game/Engine/Gym.cs
@@ -2,14 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Game.UI;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Game.Engine
 {
-    class Gym : Building
+    class Gym : Sprite
     {
-        public Gym(string name) : base(name)
+        public Gym(Texture2D tex, Rectangle area) : base(tex,area)
         {
 
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
         }
     }
 }
--- trunk/Game/Game/Engine/Item.cs
+++ trunk/Game/Game/Engine/Item.cs
@@ -1,17 +1,23 @@
-﻿namespace Game.Engine
+﻿using Game.UI;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Game.Engine
 {
-    public class Item : Object
+    public class Item : Sprite
     {
         private int level;
         private int effect;
+        private string name;
 
         /// <summary>
         /// Author: Ahmed Shirin.
         /// </summary>
-        public Item(int level, string name): base(name)
+        public Item(Texture2D tex, Rectangle area, int level, string name) : base(tex, area)
         {
+            this.name = name;
             this.level = level;
-            this.effect=10*level;
+            this.effect = 10 * level;
         }
 
         public int GetEffect()
@@ -19,14 +25,19 @@
             return effect;
         }
 
-        public string GetName()
-        {
-            return base.GetName();
-        }
-
         public int GetLevel()
         {
             return level;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
         }
     }
 }

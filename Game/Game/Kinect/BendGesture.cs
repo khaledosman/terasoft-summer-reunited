e908ef2c5570dfa7a9cfa619e6ff603e06aaf642
khaled.osmaan@gmail.com <khaled.osmaan@gmail.com@13dcf251-5519-c905-372b-15839a5b83af>
2012-08-03 09:37:49 +0000

    Update issue 1 added class BendGesture and implemented the CheckGesture method to check the gesture was done satisfying the set conditions according to joint tracking

---
+++ Game/Game/Kinect/BendGesture.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Kinect;
+
+namespace Game.Kinect
+{
+    class BendGesture1 : IRelativeGestureSegment
+    {
+        public GesturePartResult CheckGesture(Skeleton skeleton)
+        {
+            if (skeleton.Joints[JointType.Head].Position.Y > skeleton.Joints[JointType.HipCenter].Position.Y)
+            {
+                if (skeleton.Joints[JointType.Head].Position.Z > (skeleton.Joints[JointType.HipCenter].Position.Z+20))
+                {
+                    return GesturePartResult.Suceed;
+                }
+                return GesturePartResult.Pausing;
+            }
+                return GesturePartResult.Fail;
+            }
+        }
+
+    }

eb04158c88938f25748186236b4eb30f30383d0b
khaled.osmaan@gmail.com <khaled.osmaan@gmail.com@13dcf251-5519-c905-372b-15839a5b83af>
2012-08-04 11:55:42 +0000

    Update issue 4 removed keyword static

--- Game/Game/Kinect/DumbbellGesture.cs
+++ Game/Game/Kinect/DumbbellGesture.cs
@@ -10,14 +10,14 @@ namespace Game.Kinect
     {
         class DumbbellGesture1 : IRelativeGestureSegment
         {
-            public static GesturePartResult CheckGesture(Skeleton skeleton)
+            public GesturePartResult CheckGesture(Skeleton skeleton)
             {
                 if ((skeleton.Joints[JointType.HandLeft].Position.Y > skeleton.Joints[JointType.ElbowLeft].Position.Y + 5) && ((skeleton.Joints[JointType.HandLeft].Position.Z > skeleton.Joints[JointType.ElbowLeft].Position.Z + 5)))
                 {
-                    if (DumbbellGesture2.CheckGesture(skeleton)==GesturePartResult.Fail)
-                    {
-                        return GesturePartResult.Suceed;
-                    }
+                    //if (DumbbellGesture2.CheckGesture(skeleton)==GesturePartResult.Fail)
+                    //{
+                    //    return GesturePartResult.Suceed;
+                    //}
                     return GesturePartResult.Pausing;
                 }
                 return GesturePartResult.Fail;
@@ -25,14 +25,14 @@ namespace Game.Kinect
         }
         class DumbbellGesture2 : IRelativeGestureSegment
         {
-            public static GesturePartResult CheckGesture(Skeleton skeleton)
+            public GesturePartResult CheckGesture(Skeleton skeleton)
             {
                 if ((skeleton.Joints[JointType.HandRight].Position.Y > skeleton.Joints[JointType.ElbowRight].Position.Y + 5) && ((skeleton.Joints[JointType.HandRight].Position.Z > skeleton.Joints[JointType.ElbowRight].Position.Z + 5)))
                 {
-                    if (DumbbellGesture1.CheckGesture(skeleton) == GesturePartResult.Fail)
-                    {
-                        return GesturePartResult.Suceed;
-                    }
+                    //if (DumbbellGesture1.CheckGesture(skeleton) == GesturePartResult.Fail)
+                    //{
+                    //    return GesturePartResult.Suceed;
+                    //}
                     return GesturePartResult.Pausing;
                 }
                 return GesturePartResult.Fail;

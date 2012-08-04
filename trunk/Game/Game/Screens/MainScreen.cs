9e1946b264139c0f9ac8222191f9a8634a5c8df4
asamgdla@gmail.com <asamgdla@gmail.com@13dcf251-5519-c905-372b-15839a5b83af>
2012-08-04 19:29:15 +0000

    update issue 23 Status: Started Modified the Main menu to add the buttons instead of images.

--- trunk/Game/Game/Screens/MainScreen.cs
+++ trunk/Game/Game/Screens/MainScreen.cs
@@ -8,14 +8,17 @@ namespace Game.Screens
     public class MainScreen : GameScreen
     {
         Sprite menu;
-        Sprite newGame;
-        Sprite instructions;
-        Sprite highscores;
-        Sprite exit;
+        Button newGame;
+        Button instructions;
+        Button highscores;
+        Button exit;
         Sprite newGameLabel;
         Sprite instructionsLabel;
         Sprite scoreslabel;
         Sprite exitLabel;
+        SpriteBatch spriteBatch;
+        ContentManager Content;
+        Game.Kinect.Kinect kinect;
 
         /// <summary>
         /// Author: Ahmed Shirin.
@@ -26,31 +29,72 @@ namespace Game.Screens
 
         }
 
-        public void Initialize()
+        public override void Initialize()
         {
-
+            newGame = new Button();
+            instructions = new Button();
+            highscores = new Button();
+            exit = new Button();
+            newGame.Initialize("Textures//new", kinect, new Vector2(85, 210), 200, 200);
+            instructions.Initialize("Textures//instructions", kinect, new Vector2(385, 210), 200, 200);
+            highscores.Initialize("Textures//highscores", kinect, new Vector2(685, 210), 200, 200);
+            exit.Initialize("Textures//exit", kinect, new Vector2(975, 210), 200, 200);
+            newGame.Clicked += new Button.ClickedEventHandler(newGame_Clicked);
+            instructions.Clicked += new Button.ClickedEventHandler(instructions_Clicked);
+            highscores.Clicked += new Button.ClickedEventHandler(highscores_Clicked);
+            exit.Clicked += new Button.ClickedEventHandler(exit_Clicked);
         }
 
-        public void LoadContent(ContentManager Content)
+        void exit_Clicked(object sender, System.EventArgs a)
         {
-            menu = new Sprite(Content.Load<Texture2D>("Textures//menu"), new Rectangle(0, 0, 1280, 720));
-            newGame = new Sprite(Content.Load<Texture2D>("Textures//new"), new Rectangle(85, 210, 200, 200));
-            instructions = new Sprite(Content.Load<Texture2D>("Textures//instructions"), new Rectangle(385, 210, 200, 200));
-            highscores = new Sprite(Content.Load<Texture2D>("Textures//highscores"), new Rectangle(685, 210, 200, 200));
-            exit = new Sprite(Content.Load<Texture2D>("Textures//exit"), new Rectangle(975, 210, 200, 200));
+           
+        }
+
+        void highscores_Clicked(object sender, System.EventArgs a)
+        {
+            ScreenManager.AddScreen(new HighScoresScreen());
+        }
+
+        void instructions_Clicked(object sender, System.EventArgs a)
+        {
+            ScreenManager.AddScreen(new InstructionsScreen());
+        }
+
+        void newGame_Clicked(object sender, System.EventArgs a)
+        {
+            ScreenManager.AddScreen(new PlayScreen());
+        }
+
+        public override void LoadContent()
+        {
+            Content = ScreenManager.Game.Content;
+            spriteBatch = ScreenManager.SpriteBatch;
+            newGame.LoadContent(Content);
+            instructions.LoadContent(Content);
+            highscores.LoadContent(Content);
+            exit.LoadContent(Content);
+            menu = new Sprite(Content.Load<Texture2D>("Textures//menu"), new Rectangle(0, 0, 1280, 720));           
             newGameLabel = new Sprite(Content.Load<Texture2D>("Textures//new_label"), new Rectangle(70, 430, 240, 50));
             instructionsLabel = new Sprite(Content.Load<Texture2D>("Textures//instructions_label"), new Rectangle(375, 430, 240, 50));
             scoreslabel = new Sprite(Content.Load<Texture2D>("Textures//scores_label"), new Rectangle(665, 430, 240, 50));
             exitLabel = new Sprite(Content.Load<Texture2D>("Textures//exit_label"), new Rectangle(960, 430, 240, 50));
         }
 
-        public void Draw(SpriteBatch spriteBatch)
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
         {
             menu.Draw(spriteBatch);
+            SpriteBatch sprite = spriteBatch;
+            sprite.Begin();
             newGame.Draw(spriteBatch);
             instructions.Draw(spriteBatch);
             highscores.Draw(spriteBatch);
             exit.Draw(spriteBatch);
+            sprite.End();            
             newGameLabel.Draw(spriteBatch);
             instructionsLabel.Draw(spriteBatch);
             scoreslabel.Draw(spriteBatch);

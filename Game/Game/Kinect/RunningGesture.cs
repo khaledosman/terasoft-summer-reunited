8256633764d5cc5e5ab7c5d467e8a43b463dfa82
khaled.osmaan@gmail.com <khaled.osmaan@gmail.com@13dcf251-5519-c905-372b-15839a5b83af>
2012-08-04 11:32:46 +0000

    Update issue 5 Added class RunningGesture which includes checking the gesture needed to run the treadmill

---
+++ Game/Game/Kinect/RunningGesture.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Kinect;
+
+namespace Game.Kinect
+{
+   public class RunningGesture
+    {
+       static double posY;
+       class RunningGesture1 : IRelativeGestureSegment
+       {
+           public GesturePartResult CheckGesture(Skeleton skeleton)
+           {
+               posY = skeleton.Joints[JointType.HipCenter].Position.Y;
+               if (skeleton.Joints[JointType.KneeLeft].Position.Y > (skeleton.Joints[JointType.KneeRight].Position.Y+10))
+               {
+                   return GesturePartResult.Suceed;
+               }
+               return GesturePartResult.Fail;
+           }
+       }
+       class RunningGesture2 : IRelativeGestureSegment
+       {
+           public GesturePartResult CheckGesture(Skeleton skeleton)
+           {
+               if (skeleton.Joints[JointType.KneeRight].Position.Y > (skeleton.Joints[JointType.KneeLeft].Position.Y + 10))
+               {
+                   if (skeleton.Joints[JointType.HipCenter].Position.Y > (posY + 5))
+                   {
+                       return GesturePartResult.Suceed;
+                   }
+                   return GesturePartResult.Pausing;
+               }
+               return GesturePartResult.Fail;
+           }
+       }
+    }
+}

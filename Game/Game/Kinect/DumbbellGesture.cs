2f6b3a5a4fa4a10f80ea268fec2c671739d81621
khaled.osmaan@gmail.com <khaled.osmaan@gmail.com@13dcf251-5519-c905-372b-15839a5b83af>
2012-08-04 11:51:16 +0000

    Update issue 4 Added class DumbbellGesture which check if the user is doing the right gesture to play dumbbells, might still need more constraints.

---
+++ Game/Game/Kinect/DumbbellGesture.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Kinect;
+
+namespace Game.Kinect
+{
+    public class DumbbellGesture
+    {
+        class DumbbellGesture1 : IRelativeGestureSegment
+        {
+            public static GesturePartResult CheckGesture(Skeleton skeleton)
+            {
+                if ((skeleton.Joints[JointType.HandLeft].Position.Y > skeleton.Joints[JointType.ElbowLeft].Position.Y + 5) && ((skeleton.Joints[JointType.HandLeft].Position.Z > skeleton.Joints[JointType.ElbowLeft].Position.Z + 5)))
+                {
+                    if (DumbbellGesture2.CheckGesture(skeleton)==GesturePartResult.Fail)
+                    {
+                        return GesturePartResult.Suceed;
+                    }
+                    return GesturePartResult.Pausing;
+                }
+                return GesturePartResult.Fail;
+            }
+        }
+        class DumbbellGesture2 : IRelativeGestureSegment
+        {
+            public static GesturePartResult CheckGesture(Skeleton skeleton)
+            {
+                if ((skeleton.Joints[JointType.HandRight].Position.Y > skeleton.Joints[JointType.ElbowRight].Position.Y + 5) && ((skeleton.Joints[JointType.HandRight].Position.Z > skeleton.Joints[JointType.ElbowRight].Position.Z + 5)))
+                {
+                    if (DumbbellGesture1.CheckGesture(skeleton) == GesturePartResult.Fail)
+                    {
+                        return GesturePartResult.Suceed;
+                    }
+                    return GesturePartResult.Pausing;
+                }
+                return GesturePartResult.Fail;
+            }
+        }
+
+    }
+}

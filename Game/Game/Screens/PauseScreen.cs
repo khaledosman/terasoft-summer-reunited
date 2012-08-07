4428eede45b0e7a99900c1e091432bb518c9d4db
khaled.osmaan@gmail.com <khaled.osmaan@gmail.com@13dcf251-5519-c905-372b-15839a5b83af>
2012-08-07 01:39:44 +0000

    Update issue 6 Implemented the screen design

--- Game/Game/Screens/PauseScreen.cs
+++ Game/Game/Screens/PauseScreen.cs
@@ -8,10 +8,17 @@ namespace Game.Screens
     class PauseScreen : GameScreen
     {
         SpriteBatch spriteBatch;
+        SpriteFont font;
         GraphicsDevice graphics;
         int screenWidth;
         int screenHeight;
         ContentManager content;
+        string message;
+        Texture2D gradientTexture;
+
+        public PauseScreen() { message = "You have 3 seconds to get back or you die, LOL JK"; }
+        public PauseScreen(string message) { this.message = message; }
+
         public override void LoadContent()
         {
             content = ScreenManager.Game.Content;
@@ -19,6 +26,8 @@ namespace Game.Screens
             spriteBatch = ScreenManager.SpriteBatch;
             screenHeight = graphics.Viewport.Height;
             screenWidth = graphics.Viewport.Width;
+            gradientTexture = content.Load<Texture2D>("Textures\\gradient");
+            font = content.Load<SpriteFont>("SpriteFont1");
             base.LoadContent();
         }
         public override void Update(GameTime gameTime)
@@ -29,7 +38,19 @@ namespace Game.Screens
         }
         public override void Draw(GameTime gameTime)
         {
+            Vector2 viewportSize = new Vector2(screenWidth, screenHeight);
+            Vector2 textSize = font.MeasureString(message);
+            Vector2 textPosition = (viewportSize - textSize) / 2;
+            const int hPad = 32;
+            const int vPad = 16;
+            Rectangle backgroundRectangle = new Rectangle((int)textPosition.X - hPad,
+                                                          (int)textPosition.Y - vPad,
+                                                          (int)textSize.X + hPad * 2,
+                                                          (int)textSize.Y + vPad * 2);
+
             spriteBatch.Begin();
+            spriteBatch.Draw(gradientTexture, backgroundRectangle, Color.White);
+            spriteBatch.DrawString(font, message, textPosition, Color.Orange);
             spriteBatch.End();
             base.Draw(gameTime);
         }

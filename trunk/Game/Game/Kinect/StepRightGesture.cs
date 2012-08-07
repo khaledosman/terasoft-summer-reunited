90d757f38b8e91ab0c52d9c0360861295bcc788d
khaled.osmaan@gmail.com <khaled.osmaan@gmail.com@13dcf251-5519-c905-372b-15839a5b83af>
2012-08-07 00:12:15 +0000

    Update issue 3 Added constant values which are min values between joints and min values moved etc to class Constants and used them from there instead

--- trunk/Game/Game/Kinect/StepRightGesture.cs
+++ trunk/Game/Game/Kinect/StepRightGesture.cs
@@ -1,20 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.Kinect;
+﻿using Microsoft.Kinect;
+using Game.Text;
 
 namespace Game.Kinect
 {
-    public class StepRightGesture
-    {
-        static double posX;
+        
         class StepRightGesture1 : IRelativeGestureSegment
         {
             public GesturePartResult CheckGesture(Skeleton skeleton)
             {
-                posX = skeleton.Joints[JointType.HipCenter].Position.X;
-                if ((skeleton.Joints[JointType.Head].Position.X >= skeleton.Joints[JointType.HipCenter].Position.X-2)&&(skeleton.Joints[JointType.Head].Position.X <= (skeleton.Joints[JointType.HipCenter].Position.X+2)))
+                Constants.HipPosX= skeleton.Joints[JointType.HipCenter].Position.X;
+                if ((skeleton.Joints[JointType.Head].Position.X >= skeleton.Joints[JointType.HipCenter].Position.X-2)&&
+                    (skeleton.Joints[JointType.Head].Position.X <= (skeleton.Joints[JointType.HipCenter].Position.X+2)))
                 {
                     return GesturePartResult.Suceed;
                 }
@@ -24,9 +20,9 @@ namespace Game.Kinect
             {
                 public GesturePartResult CheckGesture(Skeleton skeleton)
                 {
-                    if (skeleton.Joints[JointType.Head].Position.X >= (posX+10))
+                    if (skeleton.Joints[JointType.Head].Position.X >= (Constants.HipPosX + Constants.minimumDistanceMoved))
                     {
-                        if (skeleton.Joints[JointType.HipCenter].Position.X >=(posX+10))
+                        if (skeleton.Joints[JointType.HipCenter].Position.X >= (Constants.HipPosX + Constants.minimumDistanceMoved))
                         {
                             return GesturePartResult.Suceed;
                         }
@@ -37,5 +33,5 @@ namespace Game.Kinect
 
             }
         }
-    }
+    
 }

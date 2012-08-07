4e3547db87bc57379f7564763b25576005169969
khaled.osmaan@gmail.com <khaled.osmaan@gmail.com@13dcf251-5519-c905-372b-15839a5b83af>
2012-08-07 00:12:34 +0000

    Update issue 4 Added constant values which are min values between joints and min values moved etc to class Constants and used them from there instead

--- trunk/Game/Game/Kinect/DumbbellGesture.cs
+++ trunk/Game/Game/Kinect/DumbbellGesture.cs
@@ -1,21 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Game.Text;
 using Microsoft.Kinect;
 
 namespace Game.Kinect
 {
-    public class DumbbellGesture
-    {
         class DumbbellGesture1 : IRelativeGestureSegment
         {
             public GesturePartResult CheckGesture(Skeleton skeleton)
             {
-                if ((skeleton.Joints[JointType.HandLeft].Position.Y > skeleton.Joints[JointType.ElbowLeft].Position.Y + 5) && 
-                    ((skeleton.Joints[JointType.HandLeft].Position.Z > skeleton.Joints[JointType.ElbowLeft].Position.Z + 5)))
+                if ((skeleton.Joints[JointType.HandLeft].Position.X <= skeleton.Joints[JointType.ElbowLeft].Position.X + Constants.handElbowDiff) &&
+                        (skeleton.Joints[JointType.HandLeft].Position.X >= skeleton.Joints[JointType.ElbowLeft].Position.X - Constants.handElbowDiff) &&
+                 (skeleton.Joints[JointType.HandLeft].Position.Z > skeleton.Joints[JointType.ElbowLeft].Position.Z + (Constants.handElbowDiff/2.5)))
                 {
-                    if((skeleton.Joints[JointType.HandLeft].Position.X <= skeleton.Joints[JointType.ElbowLeft].Position.X + 5) && (skeleton.Joints[JointType.HandLeft].Position.X >= skeleton.Joints[JointType.ElbowLeft].Position.X - 5))
+                    if ((skeleton.Joints[JointType.HandLeft].Position.Y > skeleton.Joints[JointType.ElbowLeft].Position.Y + Constants.handElbowYDiff))  
                     {
                      return GesturePartResult.Suceed;
                     }
@@ -28,11 +24,11 @@ namespace Game.Kinect
         {
             public GesturePartResult CheckGesture(Skeleton skeleton)
             {
-                if ((skeleton.Joints[JointType.HandRight].Position.Y > skeleton.Joints[JointType.ElbowRight].Position.Y + 5) &&
-                    ((skeleton.Joints[JointType.HandRight].Position.Z > skeleton.Joints[JointType.ElbowRight].Position.Z + 5)))
+                if ((skeleton.Joints[JointType.HandRight].Position.X <= skeleton.Joints[JointType.ElbowRight].Position.X + Constants.handElbowDiff) &&
+                        (skeleton.Joints[JointType.HandRight].Position.X >= skeleton.Joints[JointType.ElbowRight].Position.X - Constants.handElbowDiff) &&
+                 (skeleton.Joints[JointType.HandRight].Position.Z > skeleton.Joints[JointType.ElbowRight].Position.Z + (Constants.handElbowDiff / 2.5)))
                 {
-                    if ((skeleton.Joints[JointType.HandRight].Position.X <= skeleton.Joints[JointType.ElbowRight].Position.X + 5) &&
-                        (skeleton.Joints[JointType.HandRight].Position.X >= skeleton.Joints[JointType.ElbowRight].Position.X - 5))
+                    if ((skeleton.Joints[JointType.HandRight].Position.Y > skeleton.Joints[JointType.ElbowRight].Position.Y + Constants.handElbowYDiff))  
                     {
                         return GesturePartResult.Suceed;
                     }
@@ -43,4 +39,3 @@ namespace Game.Kinect
         }
 
     }
-}

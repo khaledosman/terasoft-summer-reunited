57fc0ba3bafd85fe34589acb7165cf614afb9489
khaled.osmaan@gmail.com <khaled.osmaan@gmail.com@13dcf251-5519-c905-372b-15839a5b83af>
2012-08-07 00:14:41 +0000

    Update issue 1 2 3 4 5 Added the const values needed for kinect gestures and their flags

--- Game/Game/Text/Constants.cs
+++ Game/Game/Text/Constants.cs
@@ -9,10 +9,21 @@ namespace Game.Text
     {
         public static bool isJumping;
         public static bool isSwappingHand;
-        public bool isBending;
-        public bool isPunching;
-        public bool isDumbbell;
-        public bool isRunning;
-        public bool isSteppingRight;
+        public static bool isBending;
+        public static bool isPunching;
+        public static bool isDumbbell;
+        public static bool isRunning;
+        public static bool isSteppingRight;
+        public static double HipPosX; // Skeleton initial X position needed for step right gesture.
+        public const double minimumDistanceMoved=10; //The minimum distance the skeleton has to move on x position to recognize the gesture
+        public static double posY; //initial hip Y position to make sure the skeleton is running
+        public const double kneeDiff = 5; //minimum distance between the two knees to detect the running gesture;
+        public const double hipDiff = 2; //minimum change in hip position to be able to detect the running gesture
+        public static double posZ; //initial Z position of right hand needed for the punch gesture
+        public const double handElbowDiff=5; // tolerance in the difference between hand and elbow x,y,z positions
+        public const double minHipDiff = 3; //minimum distance the user has to crouch to detect the bend gesture
+        public const double handElbowYDiff = 5; //minimum Y distance difference between the hand and the elbow to ddetect the dumbbell playing gesture.
+
+
     }
 }

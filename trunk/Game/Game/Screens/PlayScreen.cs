8dddc05adbf5fcd35f0fd73518e386cf6582c0f7
hollaxyman@gmail.com <hollaxyman@gmail.com@13dcf251-5519-c905-372b-15839a5b83af>
2012-08-14 14:37:49 +0000

    Deleted unused files from solution

--- trunk/Game/Game/Screens/PlayScreen.cs
+++ trunk/Game/Game/Screens/PlayScreen.cs
@@ -4,7 +4,6 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Content;
 using Game.Engine;
 using Game.UI;
-using Game.Screens.Components;
 using Microsoft.Xna.Framework.Media;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework.Audio;

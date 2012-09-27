7a7db0778cd78dbf1f48fd95a5d340f77e45e1e7
asamgdla@gmail.com <asamgdla@gmail.com@13dcf251-5519-c905-372b-15839a5b83af>
2012-09-27 19:37:11 +0000

    update issue 48 Status: Started Added the new Boss class representing a boss virus.

---
+++ trunk/Game/Game/UI/Boss.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Game.UI
+{
+    public class Boss
+    {
+        private int level;
+        private int health;
+        private Texture2D texture;
+        private Rectangle border;
+
+        public Boss(int level, Texture2D texture, Rectangle border)
+        {
+            this.level = level;
+            this.health = 10 * level;
+            this.texture = texture;
+            this.border = border;
+        }
+
+        public void UpdatePosition(int value)
+        {
+            border.X += value;
+        }
+
+        public void AttackBoss(int damage)
+        {
+            health -= damage;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, border, null, Color.White);
+        }
+    }
+}

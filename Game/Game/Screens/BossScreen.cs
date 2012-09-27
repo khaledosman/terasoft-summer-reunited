75b96205ffdb16e9b0efe6cbf3ec562a69032450
asamgdla@gmail.com <asamgdla@gmail.com@13dcf251-5519-c905-372b-15839a5b83af>
2012-09-27 21:06:28 +0000

    update issue 50 Status: Started Added the Boss Screen.

---
+++ Game/Game/Screens/BossScreen.cs
@@ -0,0 +1,49 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using Game.Engine;
+using Game.Text;
+using Game.UI;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Media;
+
+namespace Game.Screens
+{
+    public class BossScreen : GameScreen
+    {
+
+        private Sprite levelPassed;
+        private bool displayRewards=true;
+
+        public override void Initialize()
+        {
+
+        }
+
+        public override void LoadContent()
+        {
+            ContentManager Content = ScreenManager.Game.Content;
+            
+            levelPassed = new Sprite(Content.Load<Texture2D>("Textures//Level"), new Rectangle(1280, 200, 800, 95), Content);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (displayRewards)
+            {
+                levelPassed.HorizontalAnimation();
+            }
+        }
+
+        public override void Draw(GameTime gametime)
+        {
+            SpriteBatch spriteBatch = ScreenManager.SpriteBatch;
+            spriteBatch.Begin();
+            levelPassed.Draw(spriteBatch);
+            spriteBatch.End();
+        }
+
+    }
+}

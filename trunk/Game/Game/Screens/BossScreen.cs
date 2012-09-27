108431a73b0970f5c3a245f737bb9066f80c7afb
asamgdla@gmail.com <asamgdla@gmail.com@13dcf251-5519-c905-372b-15839a5b83af>
2012-09-27 22:37:16 +0000

    update issue 50 Status: Started Included an animation for the sword reward.

--- trunk/Game/Game/Screens/BossScreen.cs
+++ trunk/Game/Game/Screens/BossScreen.cs
@@ -15,17 +15,21 @@ namespace Game.Screens
     {
 
         private Sprite levelPassed;
-        private bool displayRewards=true;
+        private Texture2D sword;
+        private bool displayRewards=true, swordRewarded=true, shieldRewarded=true;//change this later
+        private float rotationAngle;
+        private Rectangle swordBounds;
+
 
         public override void Initialize()
         {
-
+            swordBounds = new Rectangle(0, 400, 80, 80);
         }
 
         public override void LoadContent()
         {
             ContentManager Content = ScreenManager.Game.Content;
-            
+            sword = Content.Load<Texture2D>("Textures//sword");
             levelPassed = new Sprite(Content.Load<Texture2D>("Textures//Level"), new Rectangle(1280, 200, 800, 95), Content);
         }
 
@@ -34,6 +38,20 @@ namespace Game.Screens
             if (displayRewards)
             {
                 levelPassed.HorizontalAnimation();
+                if(swordRewarded)
+                SwordAnimation(gameTime);
+            }
+        }
+
+        private void SwordAnimation(GameTime gameTime)
+        {
+            if (swordBounds.X < 615)
+            {
+                float elapsed = (float)gameTime.ElapsedGameTime.TotalSeconds;
+                rotationAngle += elapsed + 15;
+                float circle = MathHelper.Pi * 2;
+                rotationAngle = rotationAngle % circle;
+                swordBounds.X += 10;
             }
         }
 
@@ -41,6 +59,7 @@ namespace Game.Screens
         {
             SpriteBatch spriteBatch = ScreenManager.SpriteBatch;
             spriteBatch.Begin();
+            spriteBatch.Draw(sword, swordBounds, null, Color.White, rotationAngle, new Vector2(sword.Width / 2, sword.Height / 2), SpriteEffects.None, 0f);
             levelPassed.Draw(spriteBatch);
             spriteBatch.End();
         }
